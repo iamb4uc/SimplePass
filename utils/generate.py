bc1378346bcad1c07f491ee8d94a2ad4c98b002c
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2025-03-01 00:39:05 +0530

    fresh start with go

--- utils/generate.py
+++
@@ -1,7 +0,0 @@
-import random as r
-import string as s
-
-def generatePassword(len):
-    if len <= 0:
-        raise ValueError("Password length must be greater than 0")
-    return ''.join(r.choice(s.ascii_letters + s.digits + s.punctuation ) for _ in range(len))
